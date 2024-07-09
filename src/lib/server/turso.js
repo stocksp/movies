d3ff0b53620a9d0c1949048a7a5f23b32d39cdbc
Paul Stocks <paul.stocks@gmail.com>
2024-07-09 16:25:19 -0700

    simple query working

---
+++ src/lib/server/turso.js
@@ -0,0 +1,10 @@
+// @ts-nocheck
+import { createClient } from "@libsql/client"
+import { TURSO_DB_TOKEN, TURSO_SYNC_URL } from '$env/static/private'
+
+console.log('setting up Turso', TURSO_DB_TOKEN, TURSO_SYNC_URL)
+//remote only
+export const turso = createClient({
+  url: TURSO_SYNC_URL,
+  authToken: TURSO_DB_TOKEN,
+}) 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,38 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
-</script>
+	/** @type {{
+	  movieData: Array<{
+		title: string;
+		release_date: string;
+		name: string;
+		character: string;
+		birthday: string;
+		birthplace: string;
+		picture: string | null;
+	  }>;
+	}} */
+	export let data;
+  </script>
+  
 
 <svelte:head>
 	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta name="description" content="Cap' movies" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+{#if data.movieData.length === 0}
+	<p>Waiting on data...</p>
+{:else}
+	{#each data.movieData as record}
+		<p>
+			Title: {record.title}<br />
+			Release Date: {new Date(record.release_date).toLocaleDateString()}<br />
+			Actor: {record.name}<br />
+			Character: {record.character}<br />
+			Birthday: {new Date(record.birthday).toLocaleDateString()}<br />
+			Birthplace: {record.birthplace}<br />
+			{#if record.picture}
+				<img src="data:image/jpeg;base64,{record.picture}" alt={record.name} />
+			{/if}
+		</p>
+	{/each}
+{/if}
