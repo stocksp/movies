2eb5556c7735181591f14f18fcf32c0d9844d160
Paul Stocks <paul.stocks@gmail.com>
2024-07-10 10:31:54 -0700

    Added BootStrap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,16 @@
 <script>
+	import {
+		Container,
+		Card,
+		CardBody,
+		CardFooter,
+		CardHeader,
+		CardSubtitle,
+		CardText,
+		Row,
+		Col,
+		CardTitle
+	} from '@sveltestrap/sveltestrap';
 	/** @type {{
 	  movieData: Array<{
 		title: string;
@@ -11,28 +23,44 @@
 	  }>;
 	}} */
 	export let data;
-  </script>
-  
+</script>
 
 <svelte:head>
 	<title>Home</title>
 	<meta name="description" content="Cap' movies" />
 </svelte:head>
 
-{#if data.movieData.length === 0}
-	<p>Waiting on data...</p>
-{:else}
-	{#each data.movieData as record}
-		<p>
-			Title: {record.title}<br />
-			Release Date: {new Date(record.release_date).toLocaleDateString()}<br />
-			Actor: {record.name}<br />
-			Character: {record.character}<br />
-			Birthday: {new Date(record.birthday).toLocaleDateString()}<br />
-			Birthplace: {record.birthplace}<br />
-			{#if record.picture}
-				<img src="data:image/jpeg;base64,{record.picture}" alt={record.name} />
-			{/if}
-		</p>
-	{/each}
-{/if}
+<Container fluid>
+	{#if data.movieData.length === 0}
+		<p>Waiting on data...</p>
+	{:else}
+		<h3>{data.movieData[0].title}</h3>
+		{#each data.movieData as record}
+			<Card>
+				<Row>
+					<Col md="4">
+						<img
+							src="data:image/jpeg;base64,{record.picture}"
+							alt={record.name}
+							class="img-fluid rounded-start"
+						/>
+					</Col>
+					<Col md="8">
+						<CardTitle>{record.name}</CardTitle>
+						<CardText>{record.character}</CardText>
+					</Col>
+				</Row>
+			</Card>
+			<!-- <p>
+				Release Date: {new Date(record.release_date).toLocaleDateString()}<br />
+				Actor: {record.name}<br />
+				Character: {record.character}<br />
+				Birthday: {new Date(record.birthday).toLocaleDateString()}<br />
+				Birthplace: {record.birthplace}<br />
+				{#if record.picture}
+					<img src="data:image/jpeg;base64,{record.picture}" alt={record.name} />
+				{/if}
+			</p> -->
+		{/each}
+	{/if}
+</Container>
