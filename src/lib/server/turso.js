d307fce1b1cffe82a6fd056f3fe517552e65bfef
Paul Stocks <paul.stocks@gmail.com>
2024-07-09 17:17:39 -0700

    opps no no

--- src/lib/server/turso.js
+++ src/lib/server/turso.js
@@ -2,7 +2,7 @@
 import { createClient } from "@libsql/client"
 import { TURSO_DB_TOKEN, TURSO_SYNC_URL } from '$env/static/private'
 
-console.log('setting up Turso', TURSO_DB_TOKEN, TURSO_SYNC_URL)
+console.log('setting up Turso')
 //remote only
 export const turso = createClient({
   url: TURSO_SYNC_URL,
