0d4a3a26e4dbaf92fd0e893e5e381db115d96206
Cap Stocks <cap.stocks@gmail.com>
2024-07-12 09:16:30 -0700

    work with search cards

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -37,25 +37,25 @@
 	{:else}
 		
 		{#each data.movieData as record}
-			<Card>
-				<Row>
-					<Col md="8">
-						<CardTitle>{record.title}</CardTitle>
-						<CardText
-							><b>{new Date(record.release_date).getFullYear()}</b>
-							({record.runtime} min)
-							{record.overview.substring(0, 150)}</CardText
-						>
-					</Col>
-					<Col md="1">
-						<img
-							src="data:image/jpeg;base64,{record.poster}"
-							alt={record.poster}
-							class="img-fluid rounded-start"
-						/>
-					</Col>
-				</Row>
-			</Card>
+		<Card style="height: 100px; padding: 5px;">
+			<Row class="h-100">
+				<Col md="9" class="d-flex flex-column">
+					<CardTitle class="mb-1 text-truncate">{record.title}</CardTitle>
+					<CardText class="small mb-0" style="font-size: 0.8rem; line-height: 1.2;">
+						<b>{new Date(record.release_date).getFullYear()}</b>
+						({record.runtime} min)
+						{record.overview}
+					</CardText>
+				</Col>
+				<Col md="3" class="d-flex justify-content-end align-items-center p-0">
+					<img
+						src="data:image/jpeg;base64,{record.poster}"
+						alt={record.title}
+						style="height: 90px; width: auto; object-fit: contain;"
+					/>
+				</Col>
+			</Row>
+		</Card>
 		{/each}
 	{/if}
 </Container>
