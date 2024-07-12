6621426592195ad85b8e622e81ad4c71de0c5e5a
Cap Stocks <cap.stocks@gmail.com>
2024-07-12 10:11:49 -0700

    formatted search for iphone

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -37,21 +37,22 @@
 	{:else}
 		
 		{#each data.movieData as record}
-		<Card style="height: 100px; padding: 5px;">
+		<Card style="height: 100px; padding: 5px; margin-bottom: 10px;">
 			<Row class="h-100">
-				<Col md="9" class="d-flex flex-column">
-					<CardTitle class="mb-1 text-truncate">{record.title}</CardTitle>
-					<CardText class="small mb-0" style="font-size: 0.8rem; line-height: 1.2;">
+				<Col xs="10" class="d-flex flex-column pr-0">
+					<CardTitle class="large mb-0 text-truncate card-title">{record.title}</CardTitle>
+					<CardText class="small mb-0 card-text">
 						<b>{new Date(record.release_date).getFullYear()}</b>
 						({record.runtime} min)
-						{record.overview}
+						{record.overview.substring(0, 80)}...
 					</CardText>
 				</Col>
-				<Col md="3" class="d-flex justify-content-end align-items-center p-0">
+				<Col xs="2" class="d-flex justify-content-end align-items-center pl-0">
 					<img
 						src="data:image/jpeg;base64,{record.poster}"
 						alt={record.title}
-						style="height: 90px; width: auto; object-fit: contain;"
+						class="movie-poster"
+						style="height: 90px; object-fit: contain;"
 					/>
 				</Col>
 			</Row>
