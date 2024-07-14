53a2d49712e1b4acb215857d62092cfc4a4a2caa
Cap Stocks <cap.stocks@gmail.com>
2024-07-14 15:36:22 -0700

    Actor details

--- src/routes/actor/[id]/+page.svelte
+++ src/routes/actor/[id]/+page.svelte
@@ -20,3 +20,20 @@
 </script>
 
 <h1>{data.actorDetails.name}</h1>
+{#if data.actorDetails.picture}
+<img src="data:image/jpeg;base64,{data.actorDetails.picture}" alt={data.actorDetails.name} style="height: 20%; width: 20%;" />
+{:else}
+<div class="placeholder-image"></div>
+{/if}
+<h6>Born {data.actorDetails.birthday}</h6>
+<h6>in {data.actorDetails.birthplace}</h6>
+{#if data.actorDetails.deathday}
+ <h6>Died {data.actorDetails.deathday}</h6>
+{/if}
+<h6>{data.actorDetails.biography}</h6>
+<h2>Roles</h2>
+<ul>
+	{#each data.roles as role}
+		<li>As {role.character} in {role.title} released on {role.releasedate}</li>
+	{/each}
+</ul>
