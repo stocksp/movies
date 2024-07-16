7368ca07344952eab99dec6f573b01a3076af3f9
Cap Stocks <cap.stocks@gmail.com>
2024-07-16 06:22:51 -0700

    Actor age calcs

--- src/routes/actor/[id]/+page.svelte
+++ src/routes/actor/[id]/+page.svelte
@@ -15,20 +15,42 @@
     }>;
   }} */
 	export let data;
+	//console.log(JSON.stringify(data, null, 2));
 
-	console.log(JSON.stringify(data, null, 2));
+	// deal with age and death
+	let dateBirth, dateDeath, dateToday, ageInYears, span;
+	if (data.actorDetails.birthday != '' && data.actorDetails.deathday != '') {
+		dateBirth = new Date(data.actorDetails.birthday);
+		dateDeath = new Date(data.actorDetails.deathday);
+		span = dateDeath - dateBirth;
+		ageInYears = Math.floor(span / (1000 * 60 * 60 * 24 * 365.25)); // More accurate for leap years
+	} else if (data.actorDetails.birthday != '' && data.actorDetails.deathday == '') {
+		dateToday = new Date();
+		dateToday.setHours(0, 0, 0, 0);
+		dateBirth = new Date(data.actorDetails.birthday);
+		span = dateToday - dateBirth;
+		ageInYears = Math.floor(span / (1000 * 60 * 60 * 24 * 365.25)); // More accurate for leap years
+	}
 </script>
 
 <h1>{data.actorDetails.name}</h1>
 {#if data.actorDetails.picture}
-<img src="data:image/jpeg;base64,{data.actorDetails.picture}" alt={data.actorDetails.name} style="height: 20%; width: 20%;" />
+	<img
+		src="data:image/jpeg;base64,{data.actorDetails.picture}"
+		alt={data.actorDetails.name}
+		style="height: 20%; width: 20%;"
+	/>
 {:else}
-<div class="placeholder-image"></div>
+	<div class="placeholder-image"></div>
 {/if}
-<h6>Born {data.actorDetails.birthday}</h6>
+{#if data.actorDetails.birthday && data.actorDetails.deathday}
+	<h6>Born {data.actorDetails.birthday}</h6>
+{:else}
+	<h6>Born {data.actorDetails.birthday} Age {ageInYears}</h6>
+{/if}
 <h6>in {data.actorDetails.birthplace}</h6>
-{#if data.actorDetails.deathday}
- <h6>Died {data.actorDetails.deathday}</h6>
+{#if data.actorDetails.birthday && data.actorDetails.deathday}
+	<h6>Died {data.actorDetails.deathday} Age {ageInYears}</h6>
 {/if}
 <h6>{data.actorDetails.biography}</h6>
 <h2>Roles</h2>
