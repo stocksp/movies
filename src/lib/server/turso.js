f7b76f322792d1c733e4a2daa7745ccd74de4879
Paul Stocks <paul.stocks@gmail.com>
2024-07-21 15:44:01 -0700

    No Turso

--- src/lib/server/turso.js
+++ src/lib/server/turso.js
@@ -2,21 +2,8 @@
 import { createClient } from "@libsql/client";
 import { TURSO_DB_TOKEN, TURSO_SYNC_URL } from '$env/static/private';
 
-const isProd = process.env.NODE_ENV === 'production';
-
-console.log(`Setting up Turso in ${isProd ? 'production' : 'development'} mode`);
-
 export const turso = createClient(
-  isProd
-    ? {
-        // Production configuration
-        url: TURSO_SYNC_URL,
-        authToken: TURSO_DB_TOKEN,
-      }
-    : {
-        // Development configuration
-        url: "file:local.db",
-        syncUrl: TURSO_SYNC_URL,
-        authToken: TURSO_DB_TOKEN,
-      }
+  {
+    url: "file:local.db",
+  }
 );
