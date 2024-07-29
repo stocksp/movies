3cce2709e8be8ca345e5de2669e831b99ade5dab
Paul Stocks <paul.stocks@gmail.com>
2024-07-29 08:48:48 -0700

    cors hook

---
+++ src/hooks.server.js
@@ -0,0 +1,8 @@
+export async function handle({ event, resolve }) {
+    const response = await resolve(event);
+    response.headers.set('Access-Control-Allow-Origin', 'https://movies.stocks1.dev/');
+    response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    
+    return response;
+  }
