f8c3922406e16a2d1448fa4e3d37613ff46668eb
Cap Stocks <cap.stocks@gmail.com>
2024-08-21 13:19:41 -0700

    Actor fix

--- src/routes/api/actors/+server.js
+++ src/routes/api/actors/+server.js
@@ -4,14 +4,16 @@ import { json } from '@sveltejs/kit';
 import { mysql } from '$lib/server/mysql';
 
 export async function GET({ url }) {
-  console.log('Doing it:');
   const search = url.searchParams.get('search') || '';
+
+  console.log('Doing it:', search);
 
   if (search.length >= 3) {
     try {
+      const searchTerm = `${search}%`; // Add wildcard to the end of the search term
       const [results] = await mysql.query(
         'SELECT id, name FROM actors WHERE name LIKE ?',
-        [search]);
+        [searchTerm]);
       return json(results);
     } catch (error) {
       console.error('Error querying database:', error);
